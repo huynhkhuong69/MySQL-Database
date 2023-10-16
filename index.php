77710f69798088e53547813c81b9163532ba53a5
Khuong Huynh <huynhkhuong69>
2023-10-16 11:24:25 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Database</title>
+    <link rel="stylesheet" type=text/css href="mystyle.css">
+
+    <!-- <style>
+    p {
+        font-weight: bold;
+    }
+</style> -->
+</head>
+
+<body>
+    <p><strong>Welcome to CMPE172!!<strong></p>
+</body>
+
+</html>
